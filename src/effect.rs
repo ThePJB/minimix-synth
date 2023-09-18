932f557d8dad4e91bb76a4c8c3c5cb8e611f1406
Patrick Burke <zclpatrick@gmail.com>
2023-09-19 07:57:39 +1000

    wip2

--- src/effect.rs
+++ src/effect.rs
@@ -5,8 +5,10 @@ pub trait Effect {
     fn echo(self) -> Self;
     fn delay(self) -> Self;
     fn tanh(self) -> Self;
+    fn phase(self) -> Self;
+    fn flange(self) -> Self;
 }
 
 impl Effect for Signal {
-    
+
 }
